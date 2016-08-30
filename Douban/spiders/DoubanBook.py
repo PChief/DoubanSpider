03ec869b8afb376cf29d5309a9b3a0285db59dd9
PChief <PChief@users.noreply.github.com>
2016-08-30 15:36:03 +0800

    Update DoubanBook.py
    添加各模块内容xpath，指定目录结构

--- Douban/spiders/DoubanBook.py
+++ Douban/spiders/DoubanBook.py
@@ -12,46 +12,62 @@ class DoubanBook(CrawlSpider):
     
     name = 'doubanbook'
     allowed_domains = ['douban.com', 'doubanio.com']
-    start_urls = [https://book.douban.com/top250?start=25']
+    start_urls = [https://book.douban.com/top250?start=0']
     
     """
     通过Rule规则：
     
     1、匹配书籍介绍的链接 (https://book.douban.com/subject/1084336/)
-    提取：
-    a）书籍概况
-    b）豆瓣评分
-    c）内容简介
-    d）作者简介
-    e）目录
-    f) 书评链接，再进一步提取书评
-    
-    2、匹配读书笔记链接（https://book.douban.com/subject/1770782/annotation)
-    提取:
-    a) 笔记作者（头像、昵称、链接）
-    b) 笔记标题
-    c) 笔记内容
-    d）下一页
+    依次提取：
+    a）书籍概况(原始图片、基本信息div[@id="info"])
+    b）豆瓣评分(div.rating_wrap clearbox)
+    c）内容简介(div.related_info span."all hidden" div.intro)
+    d）作者简介((//div[@class="article"]//div[@class="related_info"]//div/div[@class="intro"])[2])
+    e）目录(//div[@class="indent"][contains(@id,"short")] 或者  //*[@class="indent"][@style="display:none"])
+    f) 书评链接，再进一步提取书评(//div[@id="reviews"]/div[@id="wt_0"]/p/a/@href)
+    g) 笔记链接，再进一步提取笔记(//div[@class="ugc-mod reading-notes"]/div[@class="ft"]/p/a/@href)
     
     """
     rules = (
-        # https://book.douban.com/subject/1084336/
+        # 以书目介绍链接（ttps://book.douban.com/subject/1084336/）为起点
         Rule(LinkExtractor(allow='book\.douban\.com/subject/[0-9]*/'), callback=self.parse_book_subject, follow=True, ),
         # Another 9 pages: https://book.douban.com/top250?start=50
         Rule(LinkExtractor(allow='book\.douban\.com/top250\?start=[0-9]*'), callback=self.parse_top250_rest, follow=True),
-        
-        # annotation: https://book.douban.com/subject/1770782/annotation
-        Rule(LinkExtractor(allow='book\.douban\.com/subject/[0-9]*/annotation'), callback=self.parse_subject_annotation, follow=True),
-        # next annotation page: '?sort=rank&start=10'
-        Rule(LinkExtractor(allow='\?sort=rank&start=[0-9]*'), callback=self.parse_annotation_next, follow=True),
-    
     )
     
     def parse_book_subject(self, response):
         # extract data from book link
         print response.url
         
-        # 创建目录（小王子）前先检查目录是否存在，有可能subject的调用落后于annotation
+        # 创建目录（小王子--[法]圣埃克苏佩里）前先检查目录是否存在
+        book_name_xpath = '//div[@id="wrapper"]/h1/span/text()'
+        author_name_xpath = '//div[@id="info"]/span[1]/a/text()'
+        book_name = response.xpath(book_name_xpath).extract()[0]  # u'小王子'
+        author_name = response.xpath(author_name_xpath).extract()[0]  # u'[法] 圣埃克苏佩里'
+        book_dir = book_name + '--' + author_name  
+        if not os.path.exists(book_dir_name):
+            os.mkdir(book_dir)
+        """
+        保存文件目录结构
+        ./小王子--[法] 圣埃克苏佩里
+            小王子-[法]圣埃克苏佩里(9.0).txt
+            [法]圣埃克苏佩里简介.txt
+            小王子.jpg
+            ./书评
+                5stars--长大就笨了.txt
+                ...
+            ./笔记
+                5stars--《小王子》的笔记-第2页.txt
+                ...
+        """
+        book_profile_xpath = ''
+        book_grade_xpath = ''
+        book_intro_xpath =''
+        author_intro_xpath = ''
+        book_catalogue_xpath = ''
+        reviews_link_xpath = ''
+        annotations_link_xpath = ''
+        
         
         
     def parse_top250_rest(self, response):
@@ -60,8 +76,8 @@ class DoubanBook(CrawlSpider):
         
     def parse_subject_annotation(self, response):
         # extract annotations from page
-        print response.url
-        # 创建目录前（小王子）前先检查目录是否存在，有可能subject的调用落后于annotation
+        print response.url  # https://book.douban.com/subject/1084336/annotation
+        
         
     def parse_annotation_next(self, response):
         # for Rule matchs next pages
